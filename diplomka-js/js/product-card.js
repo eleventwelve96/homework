cfadeff28f3d843b3d30e2fa47ef2117c127fd25
eleventwelve96 <eleventwelve96@gmail.com>
2026-01-07 13:46:34 +0300

    Delete diplomka-js directory

--- diplomka-js/js/product-card.js
+++
@@ -1,8 +0,0 @@
-document.querySelectorAll(".show-more-btn").forEach((btn) => {
-  btn.addEventListener("click", () => {
-    const targetId = btn.dataset.target;
-    const hiddenCards = document.getElementById(targetId);
-    hiddenCards.classList.add("show");
-    btn.hidden = true;
-  });
-});
